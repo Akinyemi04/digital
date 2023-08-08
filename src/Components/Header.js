9152e96e238a092854dc2626a443f151a19d6a14
unknown <gabrielayoku@gmail.com>
2023-08-08 13:54:40 +0100

    initial codebase

---
+++ src/Components/Header.js
@@ -0,0 +1,11 @@
+import h_logo from './images/header_logo.png'
+
+const Header = () => {
+  return (
+    <header>
+        <img src={h_logo} alt="" />
+    </header>
+  )
+}
+
+export default Header
