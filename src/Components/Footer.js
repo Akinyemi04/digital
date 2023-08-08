d4dfc5a946264c281fc26d48ebf61e7a92f68047
unknown <gabrielayoku@gmail.com>
2023-08-09 00:31:18 +0100

    updating header

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,11 +1,58 @@
-import React from 'react'
+import logo from "./images/header_logo.png";
+import { NavLink } from "react-router-dom";
 
 const Footer = () => {
   return (
-   <footer>
-    
-   </footer>
-  )
-}
+    <footer>
+      <article className="first">
+        <img src={logo} alt="" />
+        <aside>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+          <a href=""></a>
+        </aside>
+      </article>
+      <article className="second">
+        <section className="one">
+          <p>Digital Career Academy</p>
+          <p>
+            office Address: <br /> BOS House,Olden way Buiness Park,Ballybrit
+            Galeway,H91 E65V, Ireland
+          </p>
+        </section>
+        <section>
+          <h4>Program Track</h4>
+          <NavLink> Practical Scrum Master</NavLink>
+          <NavLink>Practical Business Analysis</NavLink>
+          <NavLink> Data Analysis</NavLink>
+        </section>
+        <section>
+          <h4>Resources</h4>
+          <NavLink>About Us</NavLink>
+          <NavLink>Privacy</NavLink>
+          <NavLink> Terms and Condition</NavLink>
+          <NavLink> FAQS</NavLink>
+        </section>
+        <section className="four">
+          <h4>Contact Us</h4>
+          <p>
+            <span>+353 91724190</span>
+            <span> +353 871152980</span>
+          </p>
+          <p>info@digitalcareers.academy</p>
+          <p>
+            <span>Whatsapp</span>
+            <span>+447488818292</span>
+            <span>+1587330305</span>
+          </p>
+        </section>
+      </article>
+      <p className="base">© 2023 DCA Inc. All rights reserved.</p>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,11 +1,38 @@
-import h_logo from './images/header_logo.png'
-
+import { NavLink } from "react-router-dom";
+import h_logo from "./images/header_logo.png";
+import SearchIcon from "@mui/icons-material/Search";
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 const Header = () => {
+  const screenSize = window.screen.availWidth;
+
   return (
     <header>
+      <NavLink to="/">
         <img src={h_logo} alt="" />
+      </NavLink>
+      <nav >
+        <NavLink> About us </NavLink>
+        <NavLink className='courses'>
+          <div>
+            <p> <span>Courses</span> <ArrowDropDownIcon/> </p>
+          </div>
+        </NavLink>
+        <NavLink>Work Experience Program</NavLink>
+        <NavLink>Contact Us</NavLink>
+        <NavLink>FAQs</NavLink>
+        {screenSize > 800 && (
+          <NavLink className="taster "> Free Taster Session</NavLink>
+        )}
+        <NavLink>Log In</NavLink>
+        <span className="search"> 
+          <SearchIcon />
+        </span>
+      </nav>
+      {screenSize < 800 && (
+        <NavLink className="taster "> Free Taster Session</NavLink>
+      )}
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
