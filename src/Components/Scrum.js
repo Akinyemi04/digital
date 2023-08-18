3f918ca583476cea0d1d8bc204cbcde14f3a2482
unknown <gabrielayoku@gmail.com>
2023-08-18 19:23:20 +0100

    conflict fix

--- src/Components/Scrum.js
+++ src/Components/Scrum.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Scrum = () => {
   return (
-    <div>hello scrum where are you now i hope you're good lets make i wish</div>
+    <div>hello scrum where are you now i hope you're good lets make i wish i was with you right now</div>
   )
 }
 
